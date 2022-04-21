d58e72437c63a0d5c5c568e41da1efdaed044ee1
raoht <raohongtai@live.com>
2022-04-21 11:59:31 +0800

    none

---
+++ 198.打家劫舍.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=typescript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+function rob(nums: number[]): number {
+  if (nums.length === 0) {
+    return 0
+  }
+  if (nums.length === 1) {
+    return nums[0]
+  }
+
+  let first = nums[0]
+  let second = Math.max(nums[0], nums[1])
+  let temp = 0
+  //从第三项开始 比较i和i-2 和i-1的大小 1 3 5
+  for (let i = 2; i < nums.length; i++) {
+    temp = first
+    first = second
+    second = Math.max(second, temp + nums[i])
+  }
+  return second
+}
+// @lc code=end
+console.log(rob([2, 1, 1, 2]))
--- 70.爬楼梯.ts
+++ 70.爬楼梯.ts
@@ -7,15 +7,32 @@
 // 等于求[1,2,3,5,8,13..]等比数列的第n项
 // [p, q] = [q, r] 表示q指向上一个项 p指向再上一个项 r就是当前项
 // @lc code=start
+// function climbStairs(n: number): number {
+//   let [p, q] = [0, 0]
+//   let r = 1
+//   for (let i = 1; i <= n; i++) {
+//     // r是前一项 所以q赋值后指向前一项 在上一轮循环 q指向r 所以p赋值后是前前一项
+//     ;[p, q] = [q, r]
+//     // r修改为当前项
+//     r = p + q
+//   }
+//   return r
+// }
 function climbStairs(n: number): number {
-  let [p, q] = [0, 0]
-  let r = 1
-  for (let i = 1; i <= n; i++) {
-    // r是前一项 所以q赋值后指向前一项 在上一轮循环 q指向r 所以p赋值后是前前一项
-    ;[p, q] = [q, r]
-    // r修改为当前项
-    r = p + q
+  if (n === 1) {
+    return 1
   }
-  return r
+  if (n === 2) {
+    return 2
+  }
+  let first = 1
+  let second = 2
+  let temp = 0
+  for (let i = 3; i <= n; i++) {
+    temp = first
+    first = second
+    second = temp + first
+  }
+  return second
 }
 // @lc code=end
